7522f10a850d65e7cb3aed94f4a0fc15b4560fd8
ZhN Zhang <792358982@qq.com>
2015-06-18 16:32:17 +0800

    new in Github

---
+++ gen/com/willsung/GeniusSwipe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.willsung.GeniusSwipe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/willsung/GeniusSwipe/Main.java
@@ -0,0 +1,75 @@
+//坚持 --> 收获
+
+package com.willsung.GeniusSwipe;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Main extends Activity {
+	private boolean isExit = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+<<<<<<< HEAD
+	
+	
+	
+=======
+
+>>>>>>> try_hardEr_ZN
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		menu.add(0, 1, 1, R.string.exit);
+		menu.add(0, 2, 2, R.string.about);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		if(item.getItemId() == 1){
+			finish();
+		}
+		if(item.getItemId() == 2){
+			Toast.makeText(getApplicationContext(), "测试", Toast.LENGTH_SHORT).show();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public boolean onKeyDown(int keycode, KeyEvent event){
+		if(keycode == KeyEvent.KEYCODE_BACK){
+			if(event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0){
+				exitBy2Click();
+			}
+		}
+		return false;
+	}
+	
+	public void exitBy2Click(){
+		Timer exitb2c = null;  
+	    if (isExit == false) {  
+	        isExit = true;
+	        Toast.makeText(this, "再点击一次退出!", Toast.LENGTH_SHORT).show();  
+	        exitb2c = new Timer();  
+	        exitb2c.schedule(new TimerTask() {  
+	            @Override  
+	            public void run() {  
+	                isExit = false;
+	            }  
+	        }, 1500);
+	    } else {  
+	        finish();  
+	        System.exit(0);  
+	    }
+	}
+
+}
